1b59ec6da5cdcd0bac98487502781ec734a6268e
voiceflow-gallagan <nicolas@voiceflow.com>
2024-05-01 15:47:44 +0200

    fix: env

--- app.py
+++ app.py
@@ -67,5 +67,5 @@ async def compress_text(request: CompressRequest):
 if __name__ == "__main__":
     import uvicorn
     #port = int(os.getenv("PORT", 8000))
-    port = int(os.eviron.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
