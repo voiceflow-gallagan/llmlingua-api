9fa6a21d150c2473295968182d95e5b901a64baa
voiceflow-gallagan <nicolas@voiceflow.com>
2024-05-07 14:13:36 +0200

    add: SSL

--- app.py
+++ app.py
@@ -66,3 +66,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
+    #uvicorn.run(app, host="0.0.0.0", port=port, ssl_keyfile="key.pem", ssl_certfile="cert.pem")
