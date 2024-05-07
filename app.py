78315fa67052b3107f4bc77c8b9890221c7ed92e
voiceflow-gallagan <nicolas@voiceflow.com>
2024-05-07 11:49:03 +0200

    update: compose file

--- app.py
+++ app.py
@@ -58,5 +58,5 @@ async def compress_text(request: CompressRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
